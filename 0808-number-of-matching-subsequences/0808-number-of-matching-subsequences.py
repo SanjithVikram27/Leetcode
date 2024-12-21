d3131367d6b0bda7df485ee78a2343dc1b801c82
SANJITH VIKRAM S K <151679744+SanjithVikram27@users.noreply.github.com>
2024-12-22 02:40:25 +0530

    Time: 1112 ms (15.85%), Space: 19.7 MB (37.55%) - LeetHub

--- 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.py
+++ 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.py
@@ -3,15 +3,15 @@ from collections import Counter
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         word_freq = Counter(words)
-        count = 0
-        
-        for word, freq in word_freq.items():
-            i = 0
+        count=0
+
+        for word,freq in word_freq.items():
+            i=0
             for char in s:
-                if i < len(word) and word[i] == char:
-                    i += 1
-                if i == len(word):  # Word is a subsequence
-                    count += freq
+                if i<len(word) and word[i]==char:
+                    i+=1
+                if i==len(word):
+                    count=count+freq
                     break
-        
-        return count
+        return count 
+
