19971afa97fa3785ba00af24e0c1d68a203ae6d0
SANJITH VIKRAM S K <151679744+SanjithVikram27@users.noreply.github.com>
2025-03-04 09:38:18 +0530

    Time: 619 ms (38.63%), Space: 20.7 MB (39.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -8,7 +8,7 @@ class Solution:
             if i>0 and nums[i]==nums[i-1]:
                 continue
             
-            left,right = i+1,n-1
+            left,right=i+1,n-1
             while left<right:
                 total=nums[i]+nums[left]+nums[right]
             
@@ -16,7 +16,6 @@ class Solution:
                     triplets.append([nums[i],nums[left],nums[right]])
                     left+=1
                     right-=1
-
                     while left<right and nums[left]==nums[left-1]:
                         left+=1
                     while left<right and nums[right]==nums[right+1]:
@@ -27,7 +26,4 @@ class Solution:
                 elif total>0:
                     right-=1
         return triplets
-        
-
-
-        
+            
